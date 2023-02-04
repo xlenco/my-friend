6fb602626662cabdba676cc51cb66a5b5ba7de2e
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2023-02-04 19:38:13 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const yaml = require('js-yaml')
 const fs = require('fs')
 const { fcirclePlugin } = require('./plugins/index.js')
 let friendFiles = []
-const result = {}
+const result = [] //这个就是生成的数据结果的变量,原来是个object，也就是 {}
 const files = fs.readdirSync('./data')
 const linkList = []
 files.forEach(function (item, index) {
@@ -29,7 +29,9 @@ friendFiles.forEach((item) => {
     if (!item.disable) return item
   })
   if (content.link_list.length > 0) {
-    result[name[0]] = content
+    // 这个就是合并添加的地方，备份下原来的
+    // result[name[0]] = content
+    result.push(content)
   }
 })
 
