40acac8c67880291ebf78d05110e35aca5895c8e
Xlenco <75837117+xlenco@users.noreply.github.com>
2023-02-05 11:26:51 +0800

    Update link.js

--- src/link.js
+++ src/link.js
@@ -3,8 +3,7 @@ const fs = require('fs')
 
 let ls   = [],
     data = YML.parse(fs.readFileSync('./data/友人帐.yml').toString().replace(/(?<=rss:)\s*\n/g, ' ""\n'));
-
-data.forEach((i) => {
+data.forEach((e.i) => {
     let j = data.length-1;  //获取友链数组的范围（除了最后一栏，前面的都获取）
     if (i != j) ls = ls.concat(e.link_list) 
 });
