c38f9ed6043c41c2326a641c5a7e6b5dbc84bc5f
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2023-02-05 20:01:59 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const yaml = require('js-yaml')
 const fs = require('fs')
 const { fcirclePlugin } = require('./plugins/index.js')
 let friendFiles = []
-const result = [] //这个就是生成的数据结果的变量,原来是个object，也就是 {}
+const result = {"link_list": []} //这个就是生成的数据结果的变量
 const files = fs.readdirSync('./data')
 const linkList = []
 files.forEach(function (item, index) {
@@ -11,15 +11,16 @@ files.forEach(function (item, index) {
     friendFiles.push(item)
   }
 })
-const sortKey = process.env.sortKey || ''
-/**
- * 排序
- */
-sortKey.split(',').forEach((key) => {
-  if (key) {
-    result[key] = null
-  }
-})
+
+// const sortKey = process.env.sortKey || ''
+// /**
+//  * 排序
+//  */
+// sortKey.split(',').forEach((key) => {
+//   if (key) {
+//     result[key] = null
+//   }
+// })
 
 friendFiles.forEach((item) => {
   const name = item.split('.')
@@ -31,7 +32,9 @@ friendFiles.forEach((item) => {
   if (content.link_list.length > 0) {
     // 这个就是合并添加的地方，备份下原来的
     // result[name[0]] = content
-    result.push(content)
+    // result.push(content)
+    result["link_list"].push(content)
+    result["length"] = content.link_list.length
   }
 })
 
