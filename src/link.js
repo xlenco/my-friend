edb02ce58075960b3fbee5a53e87bfc8bec56c82
Xlenco <75837117+xlenco@users.noreply.github.com>
2023-02-05 11:01:56 +0800

    Create link.js

---
+++ src/link.js
@@ -0,0 +1,12 @@
+const YML = require('yamljs') 
+const fs = require('fs') 
+
+let ls = [], 
+    data = YML.parse(fs.readFileSync('data/友人帐.yml').toString().replace(/(?<=rss:)\s*\n/g, ' ""\n')); 
+
+data.forEach((e, i) => {
+    let j = data.length-1;
+    if (i != j) ls = ls.concat(e.link_list) 
+}); 
+fs.writeFileSync('./dist/link.json', `{"link_list": ${JSON.stringify(ls)},"length":${ls.length}}`) 
+console.log('友链文件已生成。'); 
