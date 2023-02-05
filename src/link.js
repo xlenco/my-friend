713446c89e32f7d8afffdc89d2b8e787e9f9264b
Xlenco <75837117+xlenco@users.noreply.github.com>
2023-02-05 11:32:07 +0800

    Update link.js

--- src/link.js
+++ src/link.js
@@ -1,11 +1,12 @@
-const YML = require('yamljs')
-const fs = require('fs')
+const YML = require('yamljs') 
+const fs = require('fs') 
 
-let ls   = [],
-    data = YML.parse(fs.readFileSync('./data/友人帐.yml').toString().replace(/(?<=rss:)\s*\n/g, ' ""\n'));
-data.forEach((e.i) => {
-    let j = data.length-1;  //获取友链数组的范围（除了最后一栏，前面的都获取）
+let ls = [], 
+    data = YML.parse(fs.readFileSync('data/link.yml').toString().replace(/(?<=rss:)\s*\n/g, ' ""\n')); 
+
+data.forEach((e, i) => {
+    let j = data.length-1;
     if (i != j) ls = ls.concat(e.link_list) 
-});
-fs.writeFileSync('./dist/link.json', `{"link_list": ${JSON.stringify(ls)},"length":${ls.length}}`)
-console.log('友链文件已生成。');
+}); 
+fs.writeFileSync('dist/link.json', `{"link_list": ${JSON.stringify(ls)},"length":${ls.length}}`) 
+console.log('友链文件已生成。'); 
