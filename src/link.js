60f9adb5a4aa3043f740c71a35d8bd63e170c81f
Xlenco <75837117+xlenco@users.noreply.github.com>
2023-02-05 21:47:44 +0800

    Update link.js

--- src/link.js
+++ src/link.js
@@ -1,12 +1,47 @@
-const YML = require('yamljs') 
-const fs = require('fs') 
+const yaml = require('js-yaml')
+const fs = require('fs')
+const { fcirclePlugin } = require('./plugins/index.js')
+let friendFiles = []
+const result = {"link_list": []} //这个就是生成的数据结果的变量
+const files = fs.readdirSync('./data')
+const linkList = []
+files.forEach(function (item, index) {
+  // 压缩或者bese文件是没有相对应的页面的,这里做排除
+  if (item.indexOf('.yml') !== -1) {
+    friendFiles.push(item)
+  }
+})
 
-let ls = [], 
-    data = YML.parse(fs.readFileSync('data/友人帐.yml').toString().replace(/(?<=rss:)\s*\n/g, ' ""\n')); 
+// const sortKey = process.env.sortKey || ''
+// /**
+//  * 排序
+//  */
+// sortKey.split(',').forEach((key) => {
+//   if (key) {
+//     result[key] = null
+//   }
+// })
 
-data.forEach((e, i) => {
-    let j = data.length-1;
-    if (i != j) ls = ls.concat(e.link_list) 
-}); 
-fs.writeFileSync('dist/link.json', `{"link_list": ${JSON.stringify(ls)},"length":${ls.length}}`) 
-console.log('友链文件已生成。'); 
+friendFiles.forEach((item) => {
+  const name = item.split('.')
+  const content = yaml.load(fs.readFileSync(`./data/${item}`, 'utf8'))
+  linkList.push(...content.link_list)
+  content.link_list = content.link_list.filter((item) => {
+    if (!item.disable) return item
+  })
+  if (content.link_list.length > 0) {
+    // 这个就是合并添加的地方，备份下原来的
+    // result[name[0]] = content
+    // result.push(content)
+    result["link_list"].push(content)
+    result["length"] = content.link_list.length
+  }
+})
+
+if (!fs.existsSync('./dist')) {
+  fs.mkdirSync('./dist')
+}
+
+const fcircleData = fcirclePlugin(linkList)
+fs.writeFileSync('./dist/index.json', JSON.stringify(result))
+fs.writeFileSync('./dist/fcircle.json', JSON.stringify(fcircleData))
